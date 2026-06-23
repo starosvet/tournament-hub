8f4c629605e88e17413729a671a0d55bedc9e28f
starosvet <56752655+starosvet@users.noreply.github.com>
2026-06-23 20:09:51 +0300

    Update state.js

--- state.js
+++ state.js
@@ -1,18 +1,18 @@
-const STATE_KEY = "tournament_v4_state";
-const HISTORY_KEY = "tournament_v4_history";
-
-function saveState(state) {
-  localStorage.setItem(STATE_KEY, JSON.stringify(state));
-}
+const STATE_KEY = "th_v5_state";
+const HISTORY_KEY = "th_v5_history";
 
 function loadState() {
   return JSON.parse(localStorage.getItem(STATE_KEY));
 }
 
-function saveHistory(history) {
-  localStorage.setItem(HISTORY_KEY, JSON.stringify(history));
+function saveState(state) {
+  localStorage.setItem(STATE_KEY, JSON.stringify(state));
 }
 
 function loadHistory() {
   return JSON.parse(localStorage.getItem(HISTORY_KEY)) || [];
+}
+
+function saveHistory(history) {
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(history));
 }
