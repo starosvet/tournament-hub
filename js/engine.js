f6f8ab78e70e8df65728339c13c4cec253235563
starosvet <56752655+starosvet@users.noreply.github.com>
2026-06-23 23:13:37 +0300

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,69 +1,20 @@
-function shuffle(arr){
-
-return [...arr]
-.sort(()=>Math.random()-0.5);
-
+function shuffle(arr) {
+  return [...arr].sort(() => Math.random() - 0.5);
 }
 
-
-
-function createMatches(players){
-
-
-let result=[];
-
-
-players=shuffle(players);
-
-
-for(
-let i=0;
-i<players.length;
-i+=2
-){
-
-
-if(!players[i+1])
-break;
-
-
-result.push({
-
-a:players[i],
-
-b:players[i+1],
-
-votesA:0,
-
-votesB:0,
-
-winner:null,
-
-done:false
-
-});
-
-
-}
-
-
-return result;
-
-}
-
-
-
-function createRound(players){
-
-
-return {
-
-date:new Date(),
-
-matches:
-createMatches(players)
-
-};
-
-
+function createMatches(players) {
+  let result = [];
+  let p = shuffle([...players]);
+  for (let i = 0; i < p.length; i += 2) {
+    if (!p[i + 1]) break;
+    result.push({
+      a: p[i],
+      b: p[i + 1],
+      votesA: 0,
+      votesB: 0,
+      winner: null,
+      done: false
+    });
+  }
+  return result;
 }
