a17694505b55af42579b5824c4e93b228c9cafd5
starosvet <56752655+starosvet@users.noreply.github.com>
2026-06-23 20:40:57 +0300

    Delete time.js

--- time.js
+++
@@ -1,7 +0,0 @@
-const TIME_SETTINGS = {
-  roundDuration: 60 * 1000 // 1 минута для теста (потом меняешь на 24h)
-};
-
-function roundIsOver(startTime) {
-  return Date.now() > startTime + TIME_SETTINGS.roundDuration;
-}
