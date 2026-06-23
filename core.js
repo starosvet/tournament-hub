38216c843b6bf4815c480e4e6166b90c1c1ae305
starosvet <56752655+starosvet@users.noreply.github.com>
2026-06-23 19:42:25 +0300

    Update core.js

--- core.js
+++ core.js
@@ -1,1 +1,40 @@
+let state = loadState() || {
+  round: 1,
+  maxRounds: 10,
+  players: PLAYERS,
+  matches: generateRound(PLAYERS),
+  currentMatch: 0
+};
 
+function nextMatch() {
+  state.currentMatch++;
+
+  if (state.currentMatch >= state.matches.length) {
+    nextRound();
+  }
+
+  saveState(state);
+  render();
+}
+
+function nextRound() {
+  state.round++;
+
+  if (state.round > state.maxRounds) {
+    alert("🏆 Season finished!");
+    resetState();
+    location.reload();
+    return;
+  }
+
+  state.matches = generateRound(state.players);
+  state.currentMatch = 0;
+}
+
+function vote(winnerName, loserName) {
+  let w = state.players.find(p => p.name === winnerName);
+  let l = state.players.find(p => p.name === loserName);
+
+  updateElo(w, l);
+  nextMatch();
+}
